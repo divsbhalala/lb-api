0d8e16ab9ea3990c6038a0aa4583e35d68139507
divsbhalala <dsbhalala99@gmail.com>
2017-03-22 16:38:35 +0530

    added requered field validations

--- common/models/followers.js
+++ common/models/followers.js
@@ -12,5 +12,34 @@ module.exports = function(Followers) {
 
 	//Update time before saving data
   	Followers.observe('before save', common.modifyUpdatedDate);
+  	Followers.observe('before save', function (ctx, next) {
+    var data = {};
+    if (ctx.instance) {
+      data = ctx.instance;
+    } else if (ctx.args && ctx.args.data) {
+      data = ctx.args.data;
+    }
+    else if (ctx.currentInstance) {
+      data = ctx.currentInstance
+    }
+    else {
+      next();
+    }
+    var currentUser = common.getUsers(Article.app);
+    if(_.isEmpty(data._userId) && _.isEmpty(data._communityId)){
+    	return next(common.badRequest('userId or communityId is required.'))
+    }
+
+    //TODO REASSIGN
+    if (ctx.instance) {
+      ctx.instance = data;
+    } else if (ctx.args && ctx.args.data) {
+      ctx.args.data = data;
+    }
+    else {
+      ctx.currentInstance = data
+    }
+    next();
+  });
 
 };
